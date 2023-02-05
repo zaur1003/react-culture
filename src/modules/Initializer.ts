a796f0f85c97750738f87f8aee0232d28e350637
zma <zaurmahmudov1003@gmail.com>
2023-02-05 19:48:03 +0100

    feat: folder structure and initial MR

---
+++ src/modules/Initializer.ts
@@ -0,0 +1,11 @@
+import { InitializerInterface } from "../types/types";
+
+export const main = (initConfig: InitializerInterface): void => {
+  // We need to make some checks
+  // Use getCanonicalLocales and SupportedValuesOf
+};
+
+export const reconfigure = (initConfig: InitializerInterface): void => {
+  // We need to make some checks
+  // Use getCanonicalLocales and SupportedValuesOf
+};
---
+++ src/modules/collator.ts
@@ -0,0 +1,1 @@
+// Maybe rename this file later
